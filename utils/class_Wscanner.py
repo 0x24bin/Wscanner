1ec4f350c30258cfad0fb05eb21f0e650efc58a7
王松_Striker <song@secbox.cn>
2017-01-07 10:54:27 +0800

    写了wscanner调用的类，但sqlmap还需尝试才能成功调用开始注入。

---
+++ utils/class_Wscanner.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+"""
+    WScanner, another sql injection scanner
+    Author: 王松_Striker <song@secbox.cn>
+
+"""
+
+from utils.WsproxyDb import *
+from utils.sqlmap.sqlmap_api import my_sqlmap
+
+
+class Wscanner(object):
+    def __init__(self, db_config, pid):
+        self.pid = pid
+        self.db_config = db_config
+        self.wsdb = WsproxyDb(self.db_config, self.pid)
+
+    def run(self):
+        raw = self.wsdb.get_url(0)
+        url_file = self.get_url_file(raw)
+        if my_sqlmap(url_file):
+            print "%s 存在sql注入" % url_file
+        else:
+            print "%s 不存在sql注入" % url_file
+
+    @staticmethod
+    def get_url_file(raw):
+        url_file = 'tmp/url_' + str(raw['id']) + '.txt'
+        with open(url_file, 'w') as f:
+            f.write(raw['method'] + ' ' + raw['url'] + ' HTTP/1.1' + '\n')
+            f.write(raw['headers'] + '\n')
+            f.write(raw['data'])
+        return url_file
